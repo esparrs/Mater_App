d2510f293ff82ceedb4f4cfc73a9cdfdd4387154
esparrs <seve.esparrago@gmail.com>
2013-07-10 11:00:53 -0400

    Adding initial layouts and bootstrap

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,11 @@
-def main_message
-	main_message = "HAPPY 50th BIRTHDAY!"
+include ApplicationHelper
+
+def main_title(page_title)
+	the_message = "HAPPY 50th BIRTHDAY!"
+	mom_name = "Julie Esparrago"
+	if page_title.empty?
+		the_message
+	else
+		mom_name
+	end
 end
